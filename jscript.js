849f9b8d24de331e736e204d33a251f59248d1eb
Eduardo Serafim <eduardoserafiim05@gmail.com>
2024-12-03 23:28:43 -0400

    agora ta tudo certo
    tinha colocado da forma errada o arquivo

--- jscript.js
+++ jscript.js
@@ -24,10 +24,56 @@ window.addEventListener('scroll', function() {
 window.addEventListener('scroll', function(){
     var barraCima = document.getElementsByClassName('containerCima')[0];
     
-    if (window.scrollY > 10){
+    if (window.scrollY > 100){
         barraCima.classList.add('desce-foda');
     }else{
         barraCima.classList.remove('desce-foda')
     }
 })
+
+window.addEventListener('scroll', function(){
+    var hrFoda1 = document.getElementById('hrum')
+
+    if(window.scrollY > 100){
+        hrFoda1.classList.add('agaerre-nada')
+    }else{
+        hrFoda1.classList.remove('agaerre-nada')
+    }
+
+})
+
+window.addEventListener('scroll', function(){
+    var hrFoda2 = document.getElementById('hrdois')
+
+    if(window.scrollY > 500){
+        hrFoda2.classList.add('agaerre-nada')
+    }else{
+        hrFoda2.classList.remove('agaerre-nada')
+    }
+
+})
+
+window.addEventListener('scroll', function(){
+    var hrFoda3 = document.getElementById('hrtres')
+
+    if(window.scrollY > 500){
+        hrFoda3.classList.add('agaerre-nada')
+    }else{
+        hrFoda3.classList.remove('agaerre-nada')
+    }
+
+})
+
+window.addEventListener('scroll', function(){
+    var hrFoda4 = document.getElementById('hrquatro')
+
+    if(window.scrollY > 800){
+        hrFoda4.classList.add('agaerre-nada')
+    }else{
+        hrFoda4.classList.remove('agaerre-nada')
+    }
+
+})
+
+
 
